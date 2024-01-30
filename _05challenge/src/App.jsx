d37ac8644d396ba50d20fd5371f56943358c5365
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-01-30 14:42:22 +0545

    completed a 5th challenge

--- _05challenge/src/App.jsx
+++ _05challenge/src/App.jsx
@@ -1,32 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [from, setFrom] = useState("EUR");
+  const [to, setTo] = useState("USD");
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchingCurr(){
+      setLoading(true);
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`);
+      const data = await res.json();
+      setConvertedAmount(data.rates[to]);
+      setLoading(false);
+    }
+
+    if(from === to) return setConvertedAmount(amount);
+    fetchingCurr();
+  }, [from, to, amount])
+
+  function handleFrom(e){
+    setFrom(e.target.value);
+  }
+
+  function handleTo(e){
+    setTo(e.target.value);
+  }
+
+  function handleAmount(e){
+    setAmount(e.target.value);
+  }
 
   return (
     <>
-      <div>
-      <input type="text" />
-      <select>
+      <div className='main'>
+      <input 
+        type="text"
+        value={amount}
+        onChange={handleAmount}
+      />
+      <select value={from} onChange={handleFrom} disabled={loading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={to} onChange={handleTo} disabled={loading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
     </div>
+    <p>OUTPUT : {convertedAmount > 0 && convertedAmount}</p>
     </>
   )
 }
