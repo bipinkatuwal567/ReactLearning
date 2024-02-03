60a44e6dfeb9e530d016cbf32cda459d4481713d
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-02-03 14:39:40 +0545

    added question startscreen components

--- 08ReactQuiz/src/App.jsx
+++ 08ReactQuiz/src/App.jsx
@@ -1,15 +1,75 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Loader from "./Loader";
+import Error from "./Error";
 import { Main1 } from "./Main1";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  // Status will be : "Loading", "Error", "Ready", "Active", "Finished"
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case 'dataReceived': 
+      return {
+        ...state, 
+        questions: action.payload,
+        status: "ready"
+      }
+    case 'dataFailed': 
+    return {
+      ...state, 
+      status: "error"
+    }
+    case 'start': 
+      return {
+        ...state,
+        status: "active"
+      }
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      console.log(question)
+
+      return{
+        ...state,
+        answer: action.payload,
+        points: question.correctOption === action.payload ? state.points + question.points : state.points
+      }
+    default :
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
+  const questionsLength = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+    .then(res => res.json())
+    .then(data => dispatch({
+      type: "dataReceived",
+      payload: data
+    }))
+    .catch(error => dispatch({
+      type: "dataFailed"
+    }))
+  }, [])
 
   return (
     <div className="app">
       <Header />
-      <Main1>
-        <p>1/15</p>
-        <p>Question?</p>
-      </Main1>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen dispatch={dispatch} questionsLength={questionsLength} /> }
+        {status === "active" && <Question question={questions[index]} answer={answer} dispatch={dispatch} />}
     </div>
   )
 }
---
+++ 08ReactQuiz/src/components/Options.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button 
+            key={option}
+            className={`btn btn-option ${answer === index ? 'answer' : '' } 
+            ${hasAnswered ? index === question.correctOption ? 'correct' : 'wrong': ''}
+            `}
+            onClick={() => dispatch({type: "newAnswer", payload: index})}
+            disabled={hasAnswered}
+        >
+        {option}
+        </button>
+      ))}
+    </div>
+  );
+}
