ccefc824584184315e8adeeb3a57da734726cf34
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-02-14 17:51:30 +0545

    Implemented store, structured file as redux prefers and some ui related(deposit, withdraw, loan, payloan), showing balance

--- 11redux-intro/src/App.js
+++ 11redux-intro/src/App.js
@@ -1,16 +1,24 @@
-import CreateCustomer from "./CreateCustomer";
-import Customer from "./Customer";
-import AccountOperations from "./AccountOperations";
-import BalanceDisplay from "./BalanceDisplay";
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((store) => store.customer.fullName);
+  // console.log(fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
--- 11redux-intro/src/features/customers/CreateCustomer.js
+++ 11redux-intro/src/features/customers/CreateCustomer.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCustomer } from "./customerSlice";
 
 function Customer() {
-  const [fullName, setFullName] = useState("");
-  const [nationalId, setNationalId] = useState("");
+  const [fullName, setFullName] = useState("Bipin Katuwal");
+  const [nationalId, setNationalId] = useState("23432478");
 
-  function handleClick() {}
+  // console.log(fullName);
+
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    if(!fullName || !nationalId) return;
+    dispatch(createCustomer(fullName, nationalId));
+  }
 
   return (
     <div>
