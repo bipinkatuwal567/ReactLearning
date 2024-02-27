c51f377812b9f38524236b2935747404d6ecdb47
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-02-27 18:20:49 +0545

    creating a modal for Menu

--- 13the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 13the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 // const TableHeader = styled.header`
 //   display: grid;
@@ -26,7 +27,8 @@ export default function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
         <div></div>
         <div>Cabin</div>
@@ -41,5 +43,6 @@ export default function CabinTable() {
         render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
       />
     </Table>
+    </Menus>
   );
 }
--- 13the-wild-oasis/src/ui/Menus.jsx
+++ 13the-wild-oasis/src/ui/Menus.jsx
@@ -1,6 +1,9 @@
+import React, { createContext, useContext, useState } from 'react'
+import { createPortal } from 'react-dom';
+import { HiEllipsisVertical } from 'react-icons/hi2';
 import styled from "styled-components";
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -60,3 +63,65 @@ const StyledButton = styled.button`
     transition: all 0.3s;
   }
 `;
+
+const MenusContext = createContext();
+
+function Menus({children}) {
+  const [openId, setOpenId] = useState("");
+
+  const close = () => setOpenId("");
+  const open = setOpenId;
+
+  return (
+    <MenusContext.Provider value={{openId, close, open}}>
+      {children}
+    </MenusContext.Provider>
+  )
+}
+
+function Toggle({id}) {
+  const {openId, open, close} = useContext(MenusContext);
+
+  function handleClick(){
+    console.log("HandleClick");
+    openId === "" || openId !== id ? open(id) : close();
+  }
+
+  return(
+    <StyledToggle onClick={handleClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  )
+}
+
+function List({children, id}){
+  const {openId} = useContext(MenusContext);
+
+  if(openId !== id) return null;
+
+  return createPortal(
+    <StyledList position={{x: 50, y: 50}}>
+      {children}
+    </StyledList>
+    ,
+    document.body
+  );
+} 
+
+function Button({children}){
+  return(
+    <li>
+      <StyledButton>
+      {children}
+      </StyledButton>
+    </li>
+  )
+}
+
+Menus.Menu = Menu;
+Menus.Toggle = Toggle; 
+Menus.List = List;
+Menus.Button = Button;
+
+export default Menus;
+
